c104d1101efba4a7fa2bacd57b1047d8d852f889
thetalemon <sasami0314@gmail.com>
2022-04-12 23:17:17 +0900

    :sparkles: improve output format

--- release/release.sh
+++ release/release.sh
@@ -1,3 +1,9 @@
-git log | grep "#" | awk '{print $4}'
+source ./.env
 
-curl -fsSL -H "Authorization: token XXXXX" https://api.github.com/repos/thetalemon/manasblog/pulls?state=closed?since=2020-09-12T11:25:03Z | grep -e '"number":' -e '"title":'
+# yyyy-MM-ddThh:mm:ssZ
+SINCE_DATE=2021-09-12T11:25:03Z
+
+curl -fsSL -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/thetalemon/manasblog/commits?since=$SINCE_DATE | grep '"message":' | grep '#' | awk '{print "- " $5}'
+echo "---"
+
+curl -fsSL -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/thetalemon/manasblog/pulls?state=closed?since=$SINCE_DATE | grep -e '"number":' -e '"title":' | sed -e 's/,//g' | sed -e 's/"//g' | awk '{if(NR%2)ORS=": ";else ORS="\n";print $2}' | awk '{print "- " $0}'
